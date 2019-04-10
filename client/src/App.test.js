211839b33e4cb6b33cfaa4e7766c5e8bcd81dd90
Anthony Venturini <anthonydventurini@gmail.com>
2019-04-10 12:51:21 -0700

    Adds tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from '../components/App';
+
+
+
+describe('<App />', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  
+  it('renders successfully', () => {
+    render(<App />);
+  })
+
+  it('Foul counter works', () => { 
+    const { getByText } = render(<App />);
+    const button = getByText(/foul/i);
+
+    fireEvent.click(button);
+
+    getByText(/\d/i);
+  });
+
+  it('Hit counter works', () => { 
+    const { getByText } = render(<App />);
+    const button = getByText(/hit/i);
+
+    fireEvent.click(button);
+    
+    getByText(/\d/i);
+  });
+
+  it('Ball counter works', () => { 
+    const { getByText } = render(<App />);
+    const button = getByText(/ball/i);
+
+    fireEvent.click(button);
+    
+    getByText(/\d/i);
+  });
+
+  it('Strike counter works', () => { 
+    const { getByText } = render(<App />);
+    const button = getByText(/strike/i);
+
+    fireEvent.click(button);
+    
+    getByText(/\d/i);
+  });
+
 });
+
+
